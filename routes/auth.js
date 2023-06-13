fa35c41d264744e00d9bc4f4834e6b096f5afde7
suhanasalma <108382046+suhanasalma@users.noreply.github.com>
2023-06-13 23:28:20 +0600

    gogle login

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const passport = require("passport");
+require("dotenv").config();
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+      //   cookies: req.cookies
+    });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
+});
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+
+
+module.exports = router
